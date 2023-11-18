43e8f9918693cfa0406d37cb30b169d3153743c7
verkalacheva <verkalacheva@mail.ru>
2023-11-18 07:29:45 +0300

    second table + requirements

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ from requests_html import HTML
 
 def hh_resume_online_parser():
     directory = "parsed_data"
+    #таблица по профессиям
     user_id = 0
     job_info = []
+    job_perechod_id = 0
+    job_perechod = []
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         user_id += 1
@@ -33,8 +36,8 @@ def hh_resume_online_parser():
                 item = item.text.replace('\xa0',' ')
                 clean_times.append(item)
             clean_times.pop(-1)
-            print(clean_jobs)
-            print(clean_times)
+            #print(clean_jobs)
+            #print(clean_times)
 
             # добавляю работам описание
             descriptions = soup.find_all("div", {'data-qa': "resume-block-experience-description"})
@@ -45,10 +48,15 @@ def hh_resume_online_parser():
                 clean_descriptions.append(item)
 
 
-
+            
 
             for i in range(len(clean_jobs)):
-                job_info.append({'Job':clean_jobs[i], 'User-ID':user_id, 'Working time':clean_times[i], 'Description':clean_descriptions[i]})
+                job_info.append({'Job':clean_jobs[i], 'User_ID':user_id, 'Working_time':clean_times[i], 'Description':clean_descriptions[i]})
+                if i >= len(clean_jobs)-1:
+                    continue
+                # заполняем таблицу переходов
+                job_perechod_id += 1
+                job_perechod.append({'ID_transition': job_perechod_id, 'Prof_1':clean_jobs[i], 'Prof_2':clean_jobs[i+1], 'Time_transition':clean_times[i]})
 
 
                 with open("data_frame.csv", "a", encoding="utf-8", newline='') as file:
@@ -62,6 +70,12 @@ def hh_resume_online_parser():
                     )
     with open(f"data_frame.json", "a", encoding="utf-8") as file:
         json.dump(job_info, file, indent=4, ensure_ascii=False)
+
+    with open(f"data_frame2.json", "a", encoding="utf-8") as file:
+        for prof in range(len(clean_jobs)):
+            json.dump(job_perechod, file, indent=4, ensure_ascii=False)
+    
+
 
 
 
